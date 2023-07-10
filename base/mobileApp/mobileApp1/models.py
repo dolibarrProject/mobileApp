02d4ece7d0481bc69f80e60f4f11d32387bfbd9a
ismailsef05 <isefiane@DESKTOP-LADVLBR>
2023-07-10 23:10:07 +0100

    comments needs to be tackled

--- base/mobileApp/mobileApp1/models.py
+++ base/mobileApp/mobileApp1/models.py
@@ -4,7 +4,15 @@ from django.db import models
 class user(models.Model):
     userID = models.BigAutoField(primary_key=True)
     contractID = models.ForeignKey('contract', on_delete=models.CASCADE)
-    userName = models.ForeignKey
+    userName = models.ForeignKey('credentials', on_delete=models.CASCADE)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    email = models.EmailField()
+    userAddress = models.CharField(max_length=100)
+    userCity = models.CharField(max_length=100)
+    userPhoneNumber = models.CharField(max_length=100)
+    creationDate = models.DateField()
+    updateDate = models.DateField()
 
 
 class contract(models.Model):
@@ -18,7 +26,21 @@ class contract(models.Model):
         ('EXTERN','EXTERN'),
     )
 
-class Credentials(models.Model):
-    credentialId = models.AutoField(primary_key=True)
-    userName = models.CharField(max_length=100)
+class credentials(models.Model):
+    #the username is the primary key
+    userName = models.CharField(max_length=100, unique=True, primary_key=True)
     password = models.CharField(max_length=100)
+
+class supervisor(user):
+    groupRefence = models.CharField(max_length=100) #what is this?
+    assignedRegion = models.CharField(max_length=100) #could be a foreign key to region table, would be better
+    teamSize = models.IntegerField()
+    department = models.CharField(max_length=100)#could be a foreign key to department table, would be better
+
+class magazinier(user):
+
+class warehouse(models.Model):
+    warehouseID = models.BigAutoField(primary_key=True)
+    warehouseLocation = models.CharField(max_length=100)
+    warehouseCapacity = models.IntegerField() #?? capacity of what? boolean?
+    
