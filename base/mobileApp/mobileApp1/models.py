5332f0350c26bbd81d055973618dceeef9a191c4
ismailsef05 <isefiane@DESKTOP-LADVLBR>
2023-07-11 12:05:49 +0100

    corrected class casing, comments still need to be resolved

--- base/mobileApp/mobileApp1/models.py
+++ base/mobileApp/mobileApp1/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
-class user(models.Model):
+class User(models.Model):
     userID = models.BigAutoField(primary_key=True)
     contractID = models.ForeignKey('contract', on_delete=models.CASCADE)
     userName = models.ForeignKey('credentials', on_delete=models.CASCADE)
@@ -15,32 +16,41 @@ class user(models.Model):
     updateDate = models.DateField()
 
 
-class contract(models.Model):
+class Contract(models.Model):
     contractID = models.BigAutoField(primary_key=True)
     startDate = models.DateField()
     endDate = models.DateField()
     terms = (
-        ('CDI','CDI'),
-        ('CDD','CDD'),
+        ('CDI', 'CDI'),
+        ('CDD', 'CDD'),
         ('SESONIER', 'SESONIER'),
-        ('EXTERN','EXTERN'),
+        ('EXTERN', 'EXTERN'),
     )
 
-class credentials(models.Model):
-    #the username is the primary key
+
+class Credentials(models.Model):
+    # the username is the primary key
     userName = models.CharField(max_length=100, unique=True, primary_key=True)
     password = models.CharField(max_length=100)
 
-class supervisor(user):
-    groupRefence = models.CharField(max_length=100) #what is this?
-    assignedRegion = models.CharField(max_length=100) #could be a foreign key to region table, would be better
+
+class Supervisor(User):
+    groupRefence = models.CharField(max_length=100)
+    # what is this?
+    assignedRegion = models.CharField(max_length=100)
+    # could be a foreign key to region table, would be better
     teamSize = models.IntegerField()
-    department = models.CharField(max_length=100)#could be a foreign key to department table, would be better
+    department = models.CharField(max_length=100)
+    # could be a foreign key to department table, would be better
 
-class magazinier(user):
 
-class warehouse(models.Model):
+class Magazinier(User):
+    ismail = models.EmailField()
+    # fake field, not in the database
+
+
+class Warehouse(models.Model):
     warehouseID = models.BigAutoField(primary_key=True)
     warehouseLocation = models.CharField(max_length=100)
-    warehouseCapacity = models.IntegerField() #?? capacity of what? boolean?
-    
+    warehouseCapacity = models.IntegerField() 
+    # ?? capacity of what? boolean?
